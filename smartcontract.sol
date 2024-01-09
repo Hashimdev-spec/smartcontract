fb7d6fb80740ef93cf04b891cc6638e45c733d82
Hashim Aliyu Hashim <60330261+Hashimdev-spec@users.noreply.github.com>
2024-01-09 20:40:10 +0100

    Update smartcontract.sol

--- smartcontract.sol
+++ smartcontract.sol
@@ -1,30 +1,52 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.0;
 
-contract simplestorage {
-   uint public storedData;
+contract AccessControlledStorage {
+    uint public storedData;
+    address public owner;
+    mapping(address => bool) public admins;
 
-   event DataStored (
-      uint data
-   );
+    event AdminAdded(address indexed admin);
+    event AdminRemoved(address indexed admin);
+    event DataStored(uint data);
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action.");
+        _;
+    }
+    
+    modifier onlyAdmin() {
+        require(admins[msg.sender], "Only an admin can perform this action.");
+        _;
+    }
 
-   constructor(uint initVal) public {
-      storedData = initVal;
-   }
+    constructor() public {
+        owner = msg.sender; // Set the contract creator as the owner
+        admins[msg.sender] = true; // The owner is also an admin
+    }
 
-   function set(uint x) public returns (uint value) {
-      require(x < 100, "Value can not be over 100");
-      storedData = x;
+    // Add a new admin
+    function addAdmin(address _admin) public onlyOwner {
+        require(!admins[_admin], "Address is already an admin.");
+        admins[_admin] = true;
+        emit AdminAdded(_admin);
+    }
+    
+    // Remove an existing admin
+    function removeAdmin(address _admin) public onlyOwner {
+        require(admins[_admin], "Address is not an admin.");
+        admins[_admin] = false;
+        emit AdminRemoved(_admin);
+    }
 
-      emit DataStored(x);
+    // Set a new value, only callable by admins
+    function set(uint _data) public onlyAdmin {
+        require(_data < 1000, "Value can not be over 1000");
+        storedData = _data;
+        emit DataStored(_data);
+    }
 
-      return storedData;
-   }
-
-   function get() public view returns (uint retVal) {
-      return storedData;
-   }
-
-   function query() public view returns (uint retVal) {
-      return storedData;
-   }
+    // Get the stored value, callable by anyone
+    function get() public view returns (uint _retVal) {
+        return storedData;
+    }
 }
